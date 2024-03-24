8e3da952588673980228514d8a42ede18de03e12
egolanang <162660817+egolanang@users.noreply.github.com>
2024-03-24 13:35:38 +0700

    MataKuliah.java

---
+++ MataKuliah.java
@@ -0,0 +1,31 @@
+
+
+public class MataKuliah {
+    private String mk_kode;
+    private String mk_nama;
+    private String mk_nilai;
+
+    public String getMk_kode() {
+        return mk_kode;
+    }
+
+    public void setMk_kode(String mk_kode) {
+        this.mk_kode = mk_kode;
+    }
+
+    public String getMk_nama() {
+        return mk_nama;
+    }
+
+    public void setMk_nama(String mk_nama) {
+        this.mk_nama = mk_nama;
+    }
+
+    public String getMk_nilai() {
+        return mk_nilai;
+    }
+
+    public void setMk_nilai(String mk_nilai) {
+        this.mk_nilai = mk_nilai;
+    }
+}
